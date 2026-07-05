104b2122d9077246bb0c08718e6cfe8c16874cc8
daailyloot <priyansh9929@gmail.com>
2026-07-05 14:25:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+# Amazon
+AFFILIATE_TAG = "daailyloot-21"
+
+# Filters
+MIN_DISCOUNT = 50
+MIN_RATING = 4.0
+MIN_REVIEWS = 100
+
+# Scheduler
+CHECK_INTERVAL = 10  # minutes
+
+# Posting
+MAX_POSTS_PER_RUN = 3
