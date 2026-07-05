afa9c734c3de3395a97164d67d86dc9513451c24
daailyloot <priyansh9929@gmail.com>
2026-07-05 14:33:26 +0530

    Create deal_fetcher.py

---
+++ deal_fetcher.py
@@ -0,0 +1,21 @@
+def fetch_deals():
+    return [
+        {
+            "id": "deal001",
+            "title": "boAt Airdopes 311 Pro",
+            "price": 999,
+            "discount": 67,
+            "rating": 4.2,
+            "reviews": 12456,
+            "url": "https://www.amazon.in/dp/B0XXXXXXX"
+        },
+        {
+            "id": "deal002",
+            "title": "Samsung 25W Charger",
+            "price": 799,
+            "discount": 45,
+            "rating": 4.6,
+            "reviews": 3520,
+            "url": "https://www.amazon.in/dp/B0YYYYYYY"
+        }
+    ]
