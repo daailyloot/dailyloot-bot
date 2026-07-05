a414c91f04476e49c6290318e513a7c473063946
daailyloot <priyansh9929@gmail.com>
2026-07-05 15:19:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 from deal_fetcher import fetch_deals
 from filters import is_good_deal
 from affiliate import add_affiliate_tag
-from telegram_sender import send_message
+from telegram_sender import send_photo
 from database import already_posted, mark_posted
+
 
 def run():
     deals = fetch_deals()
 
     for deal in deals:
+
         if not is_good_deal(deal):
             continue
 
         if already_posted(deal["id"]):
             continue
 
-        deal["url"] = add_affiliate_tag(deal["url"])
+        affiliate = add_affiliate_tag(deal["url"])
 
-        message = f"""🔥 <b>{deal['title']}</b>
+        caption = f"""🔥 <b>{deal['title']}</b>
 
 ⭐ {deal['rating']} ({deal['reviews']} reviews)
 
 💰 ₹{deal['price']}
 📉 {deal['discount']}% OFF
-
-🛒 <a href="{deal['url']}">Buy Now</a>
 """
 
-        send_message(message)
+        send_photo(
+            deal["image"],
+            caption,
+            affiliate
+        )
+
         mark_posted(deal["id"])
+
 
 if __name__ == "__main__":
     run()
